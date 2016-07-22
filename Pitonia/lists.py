4bf2b25899ecfa725ddbf66044b40d54caf0e8da
Marto <martin_varbanov96@abv.bg>
2016-07-22 22:27:53 +0300

    graph

---
+++ Pitonia/lists.py
@@ -0,0 +1,14 @@
+sample_list = ["3123", 23, 12, "gosho"];
+
+for x, i in enumerate(sample_list):
+	print(sample_list[x])
+
+#Blank matrix
+matrix = []
+for i in range(0, 5):
+	for j in range(0, 5):
+		matrix[i][j].append("")
+
+print(matrix);
+
+input("Pre...");
