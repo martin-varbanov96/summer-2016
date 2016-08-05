544aab36b9e9dad8ccaa5b6ecb85b18287870535
Marto <martin_varbanov96@abv.bg>
2016-08-05 06:11:16 +0300

    date

--- Pitonia/softuni/05/index.py
+++ Pitonia/softuni/05/index.py
@@ -34,6 +34,8 @@ $ python3    analyze.py   catalog.csv   sales-10K.csv
     2016-01-05 10:00:00+01:00: 8028.91 €
 """
 import operator
+from datetime import datetime, timezone
+import iso8601
 
 
 def price_overall(sales_dict):
@@ -73,27 +75,69 @@ def price_by_categories(sales, catalog):
 			categories_dict[current_category] = sale["price"]
 		else:
 			categories_dict[current_category] += sale["price"]
-	print(categories_dict)
-	amounts_sorted = list()
+	sorted_categories = sorted(categories_dict.items(), key=operator.itemgetter(1))
 	print("""
 	Сума на продажби по категории (top 5)
 	-----------------------------""")
 	for i in range(0, 5):
 		print("""
-			SHOES: {:.2f} €
-			""".format(23))
+			{}: {:.2f} €
+			""".format(sorted_categories[-1-i][0], sorted_categories[-1-i][1]))
 
 
-def price_by_city():
+def price_by_city(sales_dict):
 	"""
 	docs
 	"""
-	pass
-def price_by_time():
+	city_price_dict = dict()
+	for sale in sales_dict:
+		city = sale["city"]
+		price = sale["price"]
+		if city not in city_price_dict:
+			city_price_dict[city] = 0
+		city_price_dict[city] += price
+		sorted_cities = sorted(city_price_dict.items(), key=operator.itemgetter(1))
+	print("""
+
+				Сума на продажби по градове (top 5)
+				-----------------------------
+		""")
+	for i in range(0, 5):
+			print("""
+					{city}: {price:.2f} €
+						""".format(city = sorted_cities[-1-i][0], price = sorted_cities[-1-i][1]))
+
+
+def price_by_time(sales):
 	"""
-	docs
+	Сума на продажби по час (top 5)
+-----------------------------
+	2015-12-01 12:00:00+01:00: 9209.70 €
+	2016-01-17 10:00:00+02:00: 8811.59 €
+	2016-01-05 20:00:00+01:00: 8590.52 €
+	2015-12-29 20:00:00+02:00: 8270.59 €
+	2016-01-05 10:00:00+01:00: 8028.91 €
+
+	input: sales->dictionary
+	output: None
+	printing info from above
 	"""
-	pass
+	date_dict = dict()
+	for sale in sales:
+		current_date_utc = iso8601.parse_date(sale["date"]).astimezone(timezone.utc)
+		current_date_utc = current_date_utc.replace(minute=0, second=0, microsecond=0)   # с точност до час
+		if current_date_utc not in date_dict:
+			date_dict[current_date_utc] = 0
+		date_dict[current_date_utc] += sale["price"]
+	sorted_dict = sorted(date_dict.items(), key=operator.itemgetter(1))
+	print("""
+
+	Сума на продажби по час (top 5)
+-----------------------------
+		""")
+	for i in range(0, 5 ):
+		print("{date}: {price:.2f} €".format(date=str(sorted_dict[-i-1][0]), price=sorted_dict[-i-1][1]))
+
 
 def main():
 	CATALOG_NAME = "catalog.csv"
@@ -106,6 +150,8 @@ def main():
 	catalog_dicts = load_catalog(CATALOG_NAME)
 	price_overall(sales_dict)
 	price_by_categories(sales_dict, catalog_dicts)
+	price_by_city(sales_dict)
+	price_by_time(sales_dict)
 
 if __name__ == "__main__":
 	main()
