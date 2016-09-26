8e6b267ce58ddfe6a4c578f2c36da16cd70d6030
Martey <martin_varbanov96@abv.bg>
2016-09-26 13:31:18 +0300

    django

--- Pitonia/Django/hello_django/mysite/post/urls.py
+++ Pitonia/Django/hello_django/mysite/post/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+	url(r'^$', views.index, name='index'),
+	# url(r'^(?P<thread_id>[0-9]+)/$', views.detail, name='detail'),
+	# url(r'^(?P<page_id>[0-9]+)/$', views.detail, name='detail'),
+
 ]
--- Pitonia/Django/hello_django/mysite/post/views.py
+++ Pitonia/Django/hello_django/mysite/post/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Thread,Page
+
+
 def index(request):
-	return HttpResponse("List of pages")
+	latest_question_list = Thread.objects.order_by('-pub_date')[:5]
+	output = ', '.join([q.title for q in latest_question_list])
+	return HttpResponse(output)
